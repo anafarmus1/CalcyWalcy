634c819f9f0bd5c98619eb0738edbcfbf0caf7d2
Ana Farmus <anafar@pal-nat186-130-85.itap.purdue.edu>
2025-02-21 14:56:05 -0500

    Added class Main

---
+++ src/src/Main.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Welcome to my limit calculator!");
+        System.out.println("Enter your function F(x): ");
+        System.out.println("Enter a value 'a' where the limit as x approaches a of f(x) will be calculated; ");
+    }
+}
